a183ac7ac52607d088af8becab95304595b4adde
Roshan Kunwar <jungeroshan4@gmail.com>
2024-03-04 07:39:25 +0545

    insertes the urls.py

---
+++ GymManagementSystem/GymManagementSystem/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.urls import path
+from gym.views import *
+from django.conf.urls.static import static
+from django.conf import settings
+
+urlpatterns = [
+    path('', index, name='index'),
+    path('logout/',Logout, name="logout"),
+    path('user_logout/',user_logout, name="user_logout"),
+    path('user_profile/', user_profile, name="user_profile"),
+    path('user_change_password/', user_change_password, name="user_change_password"),
+    path('registration',registration, name="registration"),
+    path('user_login/',user_login, name="user_login"),
+
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
